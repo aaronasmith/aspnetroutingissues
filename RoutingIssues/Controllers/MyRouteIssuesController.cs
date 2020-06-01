9c08ae3ac408c9ea390bb2aac211a00722f64617
Aaron Smith <aasmith@cpicardgroup.com>
2020-06-01 12:20:59 -0500

    Minimizing code

--- RoutingIssues/Controllers/MyRouteIssuesController.cs
+++ RoutingIssues/Controllers/MyRouteIssuesController.cs
@@ -11,19 +11,13 @@ namespace RoutingIssues.Controllers {
     [ApiController]
     public class MyRouteIssuesController : ControllerBase {
         [HttpGet("{param1:regex(^[[0-9]]*$)}/{param2:int}", Order = 1), MapToApiVersion("3")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult<int>> GetMyItem(string param1, int param2) {
-            return Ok(1);
+        public ActionResult<string> GetMyItem(string param1, int param2) {
+            return Ok(param1 + param2);
         }
 
         [HttpGet("MyRelatedItems/{param1:int}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult<IEnumerable<int>>> GetMyRelatedItems(int param1) {
-            return Ok(new[] { 1, 2, 3 });
+        public ActionResult<IEnumerable<int>> GetMyRelatedItems(int param1) {
+            return Ok(new[] { param1 });
         }
     }
 }
